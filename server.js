a88f1b349c9d7b45e3c0b737692d01374b4913a7
Braydon Fuller <courier@braydon.com>
2015-11-06 23:32:21 -0500

    Setup bower, bitcore dependencies, cleanup.

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 'use strict';
 
 var express = require('express');
+var path = require('path');
 var app = express();
 var instanceId = process.env.NODE_APP_INSTANCE;
 
-app.use(express.static('public'));
+app.set('views', path.resolve(__dirname, './views'));
+app.engine('ejs', require('ejs').__express);
+app.use('/static', express.static(__dirname + '/static'));
+app.get('/', function(req, res) {
+  res.render('index.ejs', {});
+});
+
 app.listen(3000);
 
 module.exports = app;
+
