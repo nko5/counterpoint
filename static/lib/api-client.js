ecd92671405f5c99b8d8d4b4051aff051514e033
Braydon Fuller <zero@braydon.com>
2015-11-07 20:21:03 -0500

    Connect to database with round robin.

--- static/lib/api-client.js
+++ static/lib/api-client.js
@@ -3,15 +3,36 @@
 window.byrd.APIClient = (function() {
 
   function APIClient() {
-
+    this.servers = [];
+    this.lastServer = 0;
   }
 
+  APIClient.prototype.init = function(callback) {
+    var self = this;
+    this.servers = this._request('GET', '/servers', null, function(err, servers) {
+      if (err) {
+        return callback(err);
+      }
+      self.servers = servers;
+      callback();
+    });
+  }
+
+  APIClient.prototype.getBasePath = function() {
+    var nextServer = this.lastServer + 1;
+    if (nextServer >= this.servers.length) {
+      nextServer = 0;
+    }
+    var server = this.servers[nextServer];
+    return 'http://' + server.address + ':' + server.port + '/';
+  };
+
   APIClient.prototype.get = function(key, callback) {
-    this._request('GET', '/' + encodeURIComponent(key), {}, callback);
+    this._request('GET', this.getBasePath() + encodeURIComponent(key), {}, callback);
   };
 
   APIClient.prototype.put = function(key, value, callback) {
-    this._request('PUT', '/' + encodeURIComponent(key), { data: value }, callback);
+    this._request('PUT', this.getBasePath() + encodeURIComponent(key), { data: value }, callback);
   };
 
   APIClient.prototype._request = function(verb, path, data, callback) {
--- static/lib/app.js
+++ static/lib/app.js
@@ -11,117 +11,130 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var api = new byrd.APIClient();
 
-  shredForm.addEventListener('submit', function() {
-    var input = document.getElementById('file');
-    var file = input.files[0];
-    var shredder = new byrd.Shredder(file);
-    var blueprintName = document.getElementById('blueprint-name').value;
+  api.init(function(err) {
+    if (err) {
+      statusline.setStatus('failed', 'Unable to initialize connection to database.');
+    }
 
-    statusline.setStatus('working', 'Checking for conflicting name...');
+    shredForm.addEventListener('submit', function() {
+      var input = document.getElementById('file');
+      var file = input.files[0];
+      var shredder = new byrd.Shredder(file);
+      var blueprintName = document.getElementById('blueprint-name').value;
 
-    api.get(blueprintName, function(err, value){
-      // If there is an error that means that should mean that the key is available
-      if(err) {
-        statusline.setStatus('success', 'Name available!');
+      statusline.setStatus('working', 'Checking for conflicting name...');
 
-        shredder.shred(function(err, chunks) {
+      api.get(blueprintName, function(err, value){
+        // If there is an error that means that should mean that the key is available
+        if(err) {
+          statusline.setStatus('success', 'Name available!');
 
-          statusline.setStatus('success', 'File shredded into ' + chunks.length + '!');
+          shredder.shred(function(err, chunks) {
 
-          async.mapLimit(chunks, 4, function(chunk, done) {
-            var hash = sha256(new Buffer(chunk, 'base64')).toString('base64');
+            statusline.setStatus('success', 'File shredded into ' + chunks.length + '!');
 
-            statusline.setStatus('working', 'Distributing chunk ' + hash + '...');
+            async.mapLimit(chunks, 4, function(chunk, done) {
+              var hash = sha256(new Buffer(chunk, 'base64')).toString('base64');
 
-            api.put(hash, chunk, function(err, result) {
-              statusline.setStatus('success', 'Chunk distributed!');
-              done(err, hash);
-            });
-          }, function(err, chunkHashes) {
-            if (err) {
-                return statusline.setStatus('failed', 'Failed to distribute all chunks.');
-            }
+              statusline.setStatus('working', 'Distributing chunk ' + hash + '...');
 
-            var blueprint = new byrd.Blueprint(chunkHashes, shredder.getMetadata(), shredder.getHash());
-            var blueprintHash = sha256(new Buffer(JSON.stringify(blueprint), 'base64')).toString('base64');
-
-            statusline.setStatus('working', 'Distributing file blueprint...');
-
-            api.put(blueprintHash, JSON.stringify(blueprint), function(err, result) {
+              api.put(hash, chunk, function(err, result) {
+                statusline.setStatus('success', 'Chunk distributed!');
+                done(err, hash);
+              });
+            }, function(err, chunkHashes) {
               if (err) {
-                return statusline.setStatus('failed', 'Failed to distribute file blueprint!');
+                return statusline.setStatus('failed', 'Failed to distribute all chunks.');
               }
 
-              statusline.setStatus('working', 'Registering alias name for file blueprint...');
+              var blueprint = new byrd.Blueprint(chunkHashes, shredder.getMetadata(), shredder.getHash());
+              var blueprintHash = sha256(new Buffer(JSON.stringify(blueprint), 'base64')).toString('base64');
 
-              api.put(blueprintName, blueprintHash, function(err){
+              statusline.setStatus('working', 'Distributing file blueprint...');
+
+              api.put(blueprintHash, JSON.stringify(blueprint), function(err, result) {
                 if (err) {
-                  return statusline.setStatus('failed', 'Failed to register alias name for file blueprint!');
+                  return statusline.setStatus('failed', 'Failed to distribute file blueprint!');
                 }
-              })
 
-              statusline.setStatus('success', 'File encrpyted, shredded, and distributed. Share your alias name!');
+                statusline.setStatus('working', 'Registering alias name for file blueprint...');
+
+                api.put(blueprintName, blueprintHash, function(err){
+                  if (err) {
+                    return statusline.setStatus('failed', 'Failed to register alias name for file blueprint!');
+                  }
+                })
+
+                statusline.setStatus('success', 'File encrpyted, shredded, and distributed. Share your alias name!');
+              });
+            });
+          });
+        }
+      });
+
+
+    });
+
+    unshredForm.addEventListener('submit', function() {
+      var blueprintName = document.getElementById('unshred-blueprint-name').value;
+
+      statusline.setStatus('working', 'Querying peers for file blueprint...');
+
+      api.get(blueprintName, function(err, result){
+        if (err) {
+          return statusline.setStatus('failed', 'Failed to lookup blueprint location!');
+        }
+        if (!result || result.data) {
+          return statusline.setStatus('failed', 'Could not find data for: ' + blueprintName);
+        }
+
+        var blueprintHash = result.data;
+
+        statusline.setStatus('working', 'Querying peers for file blueprint...');
+
+        api.get(blueprintHash, function(err, result) {
+          if (err) {
+            return statusline.setStatus('failed', 'Failed to find file blueprint!');
+          }
+
+          var blueprint = JSON.parse(result.data);
+          var shredder = new byrd.Shredder();
+
+          statusline.setStatus('working', 'Got file blueprint, querying peers for chunks...');
+
+          async.map(blueprint.chunkHashes, function(hash, done) {
+            api.get(hash, done);
+          }, function(err, results) {
+            if (err) {
+              return statusline.setStatus('failed', 'Failed to resolve all file chunks!');
+            }
+
+            var chunks = results.map(function(result) {
+              return result.data;
+            });
+
+            statusline.setStatus('working', 'Assembling file chunks and decrypting...');
+
+            shredder.unshred(blueprint.fileHash, chunks, function(err, url) {
+              if (err) {
+                return statusline.setStatus('failed', 'Failed to assemble file chunks and decrypt!');
+              }
+
+              var dataURI = blueprint.metadata + ',' + url;
+
+              statusline.setStatus('success', 'File resolved! Thank you, come again!');
+              shredder.download(dataURI);
+
             });
           });
         });
-      }
+      })
+
     });
 
 
   });
 
-  unshredForm.addEventListener('submit', function() {
-    var blueprintName = document.getElementById('unshred-blueprint-name').value;
 
-    statusline.setStatus('working', 'Querying peers for file blueprint...');
-
-    api.get(blueprintName, function(err, result){
-      var blueprintHash = result.data;
-
-      if (err) {
-        return statusline.setStatus('failed', 'Failed to lookup blueprint location!');
-      }
-
-      statusline.setStatus('working', 'Querying peers for file blueprint...');
-
-      api.get(blueprintHash, function(err, result) {
-        if (err) {
-          return statusline.setStatus('failed', 'Failed to find file blueprint!');
-        }
-
-        var blueprint = JSON.parse(result.data);
-        var shredder = new byrd.Shredder();
-
-        statusline.setStatus('working', 'Got file blueprint, querying peers for chunks...');
-
-        async.map(blueprint.chunkHashes, function(hash, done) {
-          api.get(hash, done);
-        }, function(err, results) {
-          if (err) {
-            return statusline.setStatus('failed', 'Failed to resolve all file chunks!');
-          }
-
-          var chunks = results.map(function(result) {
-            return result.data;
-          });
-
-          statusline.setStatus('working', 'Assembling file chunks and decrypting...');
-
-          shredder.unshred(blueprint.fileHash, chunks, function(err, url) {
-            if (err) {
-              return statusline.setStatus('failed', 'Failed to assemble file chunks and decrypt!');
-            }
-
-            var dataURI = blueprint.metadata + ',' + url;
-
-            statusline.setStatus('success', 'File resolved! Thank you, come again!');
-            shredder.download(dataURI);
-
-          });
-        });
-      });
-    })
-
-  });
 
 });
